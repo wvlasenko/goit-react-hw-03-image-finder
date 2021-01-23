d05cc0f8dc2bb0c2f7f92ace25c6b16f11190639
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2021-01-23 11:05:54 +0200

    fix code by menthor comments

--- src/service/api.js
+++ src/service/api.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 const apiKey = '19799176-4c87c41e9b3623f80b0424cac';
+
 const fetchImageWithKeyword = (keyword, page) => {
-  return axios
-    .get(
-      `https://pixabay.com/api/?key=${apiKey}&q=${keyword}&image_type=photo&page=${page}&per_page=12`,
-    )
-    .then(response => {
-      console.log(response.data);
-      return response.data;
-    });
+  const baseUrl = `https://pixabay.com/api/?key=${apiKey}&q=${keyword}&image_type=photo&page=${page}&per_page=12`;
+  return axios.get(baseUrl).then(response => {
+    console.log(response.data);
+    return response.data;
+  });
 };
 export default fetchImageWithKeyword;
